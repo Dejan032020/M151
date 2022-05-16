f45fb7ad23cd60abdeb64bd00648f62e803204c9
Dejan Mirkovic <dejan.mirkovic@roche.com>
2022-05-16 11:30:29 +0200

    Solution File was added with Prebuilded MVC Project

---
+++ M151_WebShop/M151_WebShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(M151_WebShop.Startup))]
+namespace M151_WebShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
