2f65a26e1b8891e426fc830c35938fa3e71e9224
Dejan Mirkovic <dejan.mirkovic@roche.com>
2022-06-17 00:13:35 +0200

    Ticketsystem is implemented

---
+++ M151_WebShop/M151_WebShop.Data/Models/Criticality.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M151_WebShop.Data.Models
+{
+    public enum Criticality
+    {
+        Low,
+        Medium,
+        High
+    }
+}
---
+++ M151_WebShop/M151_WebShop.Data/Models/Ticket.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M151_WebShop.Data.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public virtual ApplicationUser User { get; set; }
+        public int UserId { get; set; }
+
+        // The two Enums are operations wich you can chause while you create your Ticket
+        public Case Case { get; set; }
+        public Criticality Criticality { get; set; }
+        public string Textarea { get; set; }
+    }
+}
